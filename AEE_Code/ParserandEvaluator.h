83fd0c0d21530a8caeb55b76cd91f2f4943f716d
LukeReicherter <142916634+LukeReicherter@users.noreply.github.com>
2024-12-03 22:36:42 -0600

    Update ParserandEvaluator.h

--- AEE_Code/ParserandEvaluator.h
+++ AEE_Code/ParserandEvaluator.h
@@ -1,32 +1,36 @@
 #ifndef PARSERANDEVALUATOR_H
 #define PARSERANDEVALUATOR_H
 
-#include <iostream>
-#include <string>
 #include <vector>
-#include <stack>
+#include <string>
 #include <stdexcept>
 
-using std::runtime_error;
-using std::string;
-using std::vector;
-using std::stack;
-
-class evaluator {
-public :
-    static double evaluate(double left, const string& op, double right);
-};
-
 class parser {
-private:
-
-    const vector<string>& tokens;
-
-    int getPrecedence(const string& op) const;
-
 public:
-    explicit parser(const vector<string>& tokens);
-    double parse();
+    // Constructor to initialize the parser with a vector of tokens
+    explicit parser(const std::vector<std::string>& inputTokens);
+
+    // Used by the main to obtain the final result of the parser
+    double evaluate();
+
+private:
+    // Stores the tokenized input of the user
+    std::vector<std::string> tokens; 
+    // Stores the current index in the vector of tokens
+    int curIndex;
+
+    // Returns the current token in the token vector
+    std::string getCurToken() const;
+    // Consumes/Advances to the next token
+    void consumeToken();
+    // Parses top priority operators
+    double parseTopPrior(); 
+    // Parses mid priority operators
+    double parseMidPrior();
+    // Parses low priority operators
+    double parseLowPrior();
+    // Starts the recursion process and handles the addition operator
+    double parseExpression();
 };
 
-#endif
+#endif // PARSER_H
